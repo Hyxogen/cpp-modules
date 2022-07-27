96064ac430df92ceee5713209267f2f6f70dd2fb
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-07-27 16:34:43 +0200

    explicit cast from c array to pointer

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -11,7 +11,7 @@ int main() {
         const int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int      *other = new int[100];
 
-        iter(arr, 10, square_and_print);
+        iter(static_cast<int *>(arr), 10, square_and_print);
 
         for (int i = 0; i < 100; ++i) {
                 other[i] = rand() % 500;
