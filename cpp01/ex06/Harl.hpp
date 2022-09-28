1e904b2186db8ef2559d10fcb82ee6cc91867272
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-09-28 10:42:33 +0200

    update harl

--- cpp01/ex06/Harl.hpp
+++ cpp01/ex06/Harl.hpp
@@ -3,12 +3,13 @@
 
 #include <string>
 
-#define HARL_INFOPROC_COUNT 8
-
 class Harl {
         typedef void (Harl::*info_proc)() const;
 
-        static const info_proc procs[HARL_INFOPROC_COUNT];
+        struct entry {
+                std::string     level;
+                Harl::info_proc proc;
+        };
 
         void debug() const;
         void info() const;
@@ -16,12 +17,11 @@ class Harl {
         void error() const;
         void unknown() const;
 
+        Harl::info_proc get_info_proc(const std::string &level) const;
+
     public:
         Harl();
 
         void complain(const std::string &level) const;
 };
-
-std::size_t hash(const std::string &str);
-
 #endif
